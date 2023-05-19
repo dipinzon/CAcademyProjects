228905b205ec4f80809b612b4e3ac0a74deb50fd
dipinzon <dipinzon@hotmail.com>
2023-05-19 11:47:20 -0400

    Changes

---
+++ JavaBasics/HelloYou.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class HelloYou{
+   public static void main(String[] args) {
+     System.out.println("Hello Diego");
+     int numComments = 6;
+     System.out.println(14%4);
+     
+   }
+}
---
+++ PhytonBasics/initials.py
@@ -0,0 +1,19 @@
+print("Hello Phyton")
+
+item = "Silla de Lujo"
+value = 250.99
+tax = 0.088 
+
+total = value * tax
+
+print(item)
+print("El valor de compra es: ")
+print(total)
+
+#####################
+
+favorite_fruit = input("What is your favorite fruit? ")
+
+print("Oh cool! I like " + favorite_fruit + " too, but I think my favorite fruit is apple.")
+#####################
+
