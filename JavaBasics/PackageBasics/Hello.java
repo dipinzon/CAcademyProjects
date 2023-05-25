90ea1c2cc9acd0017e6b39420ba864d8d2c0d22c
dipinzon <dipinzon@hotmail.com>
2023-05-25 08:49:01 -0400

    Updates

---
+++ JavaBasics/PackageBasics/Hello.java
@@ -0,0 +1,10 @@
+package PackageBasics;
+
+public class Hello{
+    public static void main(String[] args) {
+      System.out.println("Hello Diego");
+      int numComments = 6;
+      System.out.println(14%4);
+      
+    }
+ }
